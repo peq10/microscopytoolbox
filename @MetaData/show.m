ddc3dc2acf271ceb65a60566a034990efaa8a80e
rwollman <rwollman@1c5c9a67-ad30-0410-b591-754ad5106443>
2007-05-17 18:03:57 +0000

    AcqFcn directory

--- @MetaData/show.m
+++ @MetaData/show.m
@@ -1,4 +1,4 @@
-function show( md,img )
+function show( md,img,varargin )
 %SHOW an image acording to its MetaData
 %   show the image based in the levels specified in the MetaData object
 %   if img is RGB (e.g. n x m x 3) uses the rgb levels otherwise uses the 
