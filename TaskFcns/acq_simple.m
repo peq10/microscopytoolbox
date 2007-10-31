810468ece6c84514910886876d92db452294e61e
rwollman <rwollman@1c5c9a67-ad30-0410-b591-754ad5106443>
2007-10-31 23:07:40 +0000

    fixing the svn...

--- TaskFcns/acq_simple.m
+++ TaskFcns/acq_simple.m
@@ -1,28 +1,29 @@
-function acq_simple(Tsk) 
-%SIMPLEACQ callback function to be used by by AcqSeq objects
-%   
-% Outline: 
-% 1. get the current number in the sequence
-% 2. goto the right xyz position, 
-% 3. autofocus
-% 4. expose & snap an image
-% 5. save to disk
+function acq_spindleHunt(Tsk) 
 
 global rS;
 
 %% get the crnt acq details 
-[X,Y,Z,Exposure,Channels,Binning]=get(Tsk,'stageX',...
+[X,Y,Exposure,Channels,Binning]=get(Tsk,'stageX',...
                                           'stageY',...
-                                          'stageZ',...
                                           'exposuretime',...
                                           'ChannelNames',...
                                           'Binning');
+Z=guessFocalPlane(rS,X,Y);
+
 %% goto XYZ
-set(rS,'xy',[X Y],'z',Z);
+set(rS,'xy',[X Y],'z',Z,'binning',Binning);
+
+%% update status figures
 figure(1)
-plot(X,Y,'or');
+plot(X,Y,'-or');
+figure(2)
+plotFocalPlaneGrid(rS);
+
 %% autofocus
-autofocus(rS);
+autofocus(rS,1);
+z=get(rS,'z');
+z=z+get(Tsk,'zshift');
+set(rS,'z',z);
 z=get(rS,'z');
 z=z+get(Tsk,'zshift');
 set(rS,'z',z);
@@ -32,20 +33,31 @@ FcsScr.QdataType='FocusScore';
 FcsScr.Value=get(rS,'focusscore');
 FcsScr.QdataDescription='';
 
+Zguess.QdataType='FocalPlaneGuess';
+Zguess.Value=Z;
+Zguess.QdataDescription='';
+
+qdata=[FcsScr Zguess];
+
 Tsk=set(Tsk,'planetime',now,...
             'stagex',get(rS,'x'),...
             'stagey',get(rS,'y'),...
             'stagez',get(rS,'z'),...
-            'qdata',FcsScr);
-
+            'qdata',qdata);
         
-replaceTasks(rS,Tsk);
-
-%% snap images
+%% snap image
 img=acqImg(rS,Channels,Exposure);
+
+%% show image in figure 3
+figure(3)
+imshow(img(:,:,1),[],'initialmagnification','fit')
 
 %% set Task to executed and update rS
 replaceTasks(rS,set(Tsk,'executed',true));
+
+%% update Task Status
+figure(4)
+plotTaskStatus(rS)
 
 %% Write image to disk
 writeTiff(Tsk,img,get(rS,'rootfolder')); 
