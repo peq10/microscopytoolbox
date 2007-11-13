50f38ac3445acd105b94106a2801322343b3c56a
rwollman <rwollman@1c5c9a67-ad30-0410-b591-754ad5106443>
2007-11-13 11:48:26 +0000

    Getting ready for the first Fully automated Roboscope Run !!! - forgot a file...

---
+++ branches/japan-ver/ImageAnalysis/detectNEB.m
@@ -0,0 +1,44 @@
+function NEB=detectNEB(img,PlausiblyProphase)
+
+%% create a new ROI based on old circle fits and find new cells in it
+tileSize=256;
+se=strel('disk',5);
+nebTubIntRatio=0.95;
+
+NEB=zeros(length(PlausiblyProphase),1);
+
+oldCenters=unique(PlausiblyProphase(:,3:4),'rows');
+
+roi=[oldCenters(:,1)-tileSize/2 oldCenters(:,2)-tileSize/2 oldCenters(:,1)+tileSize/2 oldCenters(:,2)+tileSize/2];
+roi=ceil(roi);
+roi(:,1)=max(roi(:,1),1);
+roi(:,2)=max(roi(:,2),1);
+roi(:,3)=min(roi(:,3),size(img,2));
+roi(:,4)=min(roi(:,4),size(img,1));
+CircleFit=circleFitCells(bw,roi);
+
+%% Assign new circle to old cells
+[oldCenters,ix]=unique(PlausiblyProphase(:,3:4),'rows');
+oldRadi=PlausiblyProphase(ix,5);
+D=distance(oldCenters',CircleFit(:,1:2)');
+
+[mn,mi]=min(D);
+mi(mn>oldRadi)=NaN;
+
+%% fit nucleus to all new cells 
+
+for i=1:length(mi)
+    if ~isnan(mi(i))
+        ix=mi(i);
+        rect=[CircleFit(ix,1)-CircleFit(ix,3) CircleFit(ix,2)-CircleFit(ix,3) 2*ones(1,2)*CircleFit(ix,3)];
+        sml=imcrop(img,rect);
+        [Nuc{i},bw]=nucDetect(sml,1);
+        cyto=logical(imdilate(bw,se)-bw);
+        if isempty(Nuc{i}) || sum(sml(bw))./sum(sml(cyto)) > nebTubIntRatio
+            NEB(i)=true;
+        end
+    end
+end
+
+
+
