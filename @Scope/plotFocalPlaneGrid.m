01910fab72c07d1f220e0fd1b714632ef7f07213
rwollman <rwollman@1c5c9a67-ad30-0410-b591-754ad5106443>
2008-01-24 19:17:05 +0000

    updated the plotting methdology

--- @Scope/plotFocalPlaneGrid.m
+++ @Scope/plotFocalPlaneGrid.m
@@ -1,9 +1,5 @@
-function plotFocalPlaneGrid( rS ,fig)
+function plotFocalPlaneGrid( rS )
 %PLOTFOCALPLANEGRID plots the current known focal plane
-
-if exist('fig','var')
-    figure(fig);
-end
 
 XYZ=get(rS,'focalplane');
 if isempty(XYZ), XYZ=zeros(3,3,3); end
--- @Scope/plotRoute3D.m
+++ @Scope/plotRoute3D.m
@@ -1,10 +1,6 @@
-function plotRoute3D(rS,fig)
+function plotRoute3D(rS)
 %PLOTPLANNEDSCHEDULE Summary of this function goes here
 %   Detailed explanation goes here
-
-if ~exist('fig','var')
-    fig=figure;
-end
 
 tb=getTasks(rS,'status','executed');
 [X,Y,T]=get(tb,'stagex','stagey','planetime');
@@ -14,7 +10,6 @@ else
     xyt=[X Y 0];
 end
 
-figure(fig);
 clf
 plot3(xyt(:,1),xyt(:,2),xyt(:,3),'.-'); 
 grid on
