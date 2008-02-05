5ca9b1c8b1f837fd77b63e3116b85e4267d30254
rwollman <rwollman@1c5c9a67-ad30-0410-b591-754ad5106443>
2008-02-05 22:51:57 +0000

    making sure the demos are nice

--- Demos/dm6_combineScanWithTimelapse.m
+++ Demos/dm6_combineScanWithTimelapse.m
@@ -31,8 +31,8 @@ disp('Scope initialized');
 % We create a 5x5 grid where distance between sites is 1000 um. The
 % center of the grid is at [0 0]
 
-rows=7;
-cols=7;
+rows=5;
+cols=5;
 cntr=[0 0];
 dX=1000;
 Pos=createAcqPattern('grid',cntr,rows,cols,dX,zeros(rows*cols,1));
@@ -77,7 +77,7 @@ set(rS,'schedulingmethod','heuristicFrogLeaps')
 x=[-100 100 300];
 y=[400 135 890];
 z=[0 0 0];
-t=0:10:600;  % in seconds (that what rS is defalut units are for acquisition) 
+t=0:10:60;  % in seconds (that what rS is defalut units are for acquisition) 
 t=transformUnits(rS,'acqTime',t);
 
 %%
